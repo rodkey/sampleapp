78050aa5079cc372d06ae7a34d6e781a2ccb743b
John Rodkey <rodkey@westmont.edu>
2012-04-02 20:34:30 -0700

    make basic User model with secure paswords

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+#CASClient::Frameworks::Rails::Filter.configure(
+#  :cas_base_url => "https://cas.westmont.edu/"
+#)
